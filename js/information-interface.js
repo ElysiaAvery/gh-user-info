e75ae2a797c54eb9e0715df77d17495a63f9bd69
Elysia Nason <elysia.avery@gmail.com>
2016-10-14 08:37:15 -0700

    add moment.js formatting for the date and time that each repo was created

--- js/information-interface.js
+++ js/information-interface.js
@@ -1,7 +1,7 @@
 var Information = require('./../js/information.js').informationModule;
 
-var displayUser = function(username, repoName, repoUrl) {
-  $('#output').append('<li>' + repoName + ': ' + 'can be viewed <a href=' + repoUrl + '>here</a></li>');
+var displayUser = function(username, repoName, repoUrl, repoCreation) {
+  $('#output').append('<li>' + repoName + ': ' + 'can be viewed <a href=' + repoUrl + '>here</a></li>' + '<li>This repo was created on: ' + moment(repoCreation).format('LLLL') + '</li>');
 };
 
 $(document).ready(function() {
--- js/information.js
+++ js/information.js
@@ -5,20 +5,21 @@ function Information() {
 }
 
 Information.prototype.getRepos = function(username, displayUser){
-  $.get('https://api.github.com/users/' + username + '/repos?per_page=100?access_token=' + apiKey).then(function(response){
+  $.get('https://api.github.com/users/' + username + '/repos?per_page=1000?access_token=' + apiKey).then(function(response){
     console.log(response);
     if(response.length === 0) {
       $("#output").append("<li>There are no repos by this user.</li>");
     } else {
       for(var i = 0; i < response.length; i ++) {
-        console.log(response);
-        displayUser(username, response[i].name, response[i].html_url);
+        displayUser(username, response[i].name, response[i].html_url, response[i].created_at);
         console.log(response[i].html_url);
+        console.log(response[i].created_at);
       }
     }
   }).fail(function(error){
     console.log("there is an error.");
   });
 };
+
 
 exports.informationModule = Information;
