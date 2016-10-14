342106b51a240a7f0af36a14f1be789e550c7ff3
elysia nason <elysia.avery@gmail.com>
2016-10-13 22:52:11 -0700

    add beginning back end

--- js/information-interface.js
+++ js/information-interface.js
@@ -1,1 +1,14 @@
 var Information = require('./../js/information.js').informationModule;
+
+var displayUser = function(username, repoName, repoUrl) {
+  $('#output').append('<li>' + repoName + ': ' + 'can be viewed <a href=' + repoUrl + 'here</a></li>');
+};
+
+$(document).ready(function() {
+  $('#ghForm').submit(function() {
+    event.preventDefault();
+    var newInfo = new Information();
+    var username = $('#username').val();
+    newInfo.getRepos(username, displayUser);
+  })
+});
--- js/information.js
+++ js/information.js
@@ -1,2 +1,23 @@
+var apiKey = require('./../.env').apiKey;
+
+function Information() {
+
+}
+
+Information.prototype.getRepos = function(username, displayUser){
+  $.get('https://api.github.com/users/' + username + '/repos?per_page=100?access_token=' + apiKey).then(function(response){
+    console.log(response);
+    if(response.length === 0) {
+      $("#output").append("<li>There are no repos by this user.</li>");
+    } else {
+      for(var i = 0; i < response.length; i ++) {
+        console.log(response);
+        displayUser(username, response[i].full_name, response[i].html_url);
+      }
+    }
+  }).fail(function(error){
+    console.log("there is an error.");
+  });
+};
 
 exports.informationModule = Information;
