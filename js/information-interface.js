1a40d6ee759b7681f9ec3a0a7baf5e0747d445db
elysia nason <elysia.avery@gmail.com>
2016-10-16 12:12:18 -0700

    Add getuserinfo method

--- js/information-interface.js
+++ js/information-interface.js
@@ -2,8 +2,13 @@ var Repos = require('./../js/repos.js').reposModule;
 
 
 var displayUser = function(username, repoName, repoUrl, repoCreation, userAvatar) {
-  $('#gh-output').text('All Repositories Made By: ' + username);
-  $('#output').append('<div class="well"><li><img src="' + userAvatar + '.png"/>' + '<h3>' + repoName + '</h3> ' + 'View <a href=' + repoUrl + '>here...</a></li>' + '<li>This repo was created on: ' + moment(repoCreation).format('LLLL') + '</div></li>');
+  $('#gh-output').text('Repositories By This User: ');
+  $('#output').append('<div class="well"><li><h3>' + repoName + '</h3> ' + 'View <a href=' + repoUrl + '>here...</a></li>' + '<li>This repo was created on: ' + moment(repoCreation).format('LLLL') + '</div></li>');
+};
+
+var displayInfo = function(username, name, email, url, avatar) {
+  $('#user-header').text('User Information: ');
+  $('#info-output').append('<div class="well"><img src="' + avatar + '.png"/><h2>' + name + '</h2><h4>Email: ' + email + '</h4><h4>Visit Their GitHub page <a href="' + url + '">here...</a></h4></div>');
 };
 
 $(document).ready(function() {
@@ -12,5 +17,6 @@ $(document).ready(function() {
     var newRepos = new Repos();
     var username = $('#username').val();
     newRepos.getRepos(username, displayUser);
+    newRepos.getUserInfo(username, displayInfo);
   });
 });
--- js/repos.js
+++ js/repos.js
@@ -11,13 +11,26 @@ Repos.prototype.getRepos = function(username, displayUser){
       $("#output").append("<li>There are no repos by this user.</li>");
     } else {
       for(var i = 0; i < response.length; i ++) {
-        displayUser(username, response[i].name, response[i].html_url, response[i].created_at, response[i].owner.avatar_url);
+        displayUser(username, response[i].name, response[i].html_url, response[i].created_at);
         console.log(response[i].html_url);
         console.log(response[i].created_at);
       }
     }
   }).fail(function(error){
     $('#output').text(error.responseJSON.message);
+  });
+};
+
+Repos.prototype.getUserInfo = function(username, displayInfo){
+  $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
+    console.log(response);
+    if(response.length === 0) {
+      $("#info-output").append("<li>There is no user by this name.</li>");
+    } else {
+      displayInfo(username, response.name, response.email, response.html_url, response.avatar_url);
+    }
+  }).fail(function(error){
+    $('#info-output').text(error.responseJSON.message);
   });
 };
 
